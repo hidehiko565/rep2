17dc88a915609b1bba832313a71d2fb6b67e7486
hidehiko565 <hidehiko565@gmail.com>
2025-11-13 18:25:41 +0900

    Add functionality to save detected faces image
    Save the output image with detected faces.
    ファイル名を外部変数化

--- cv_test2.py
+++ cv_test2.py
@@ -1,5 +1,10 @@
 import cv2
 
+# 画像ファイルのパスを指定
+in_image_path = "picture2.jpg"  # ←ここを自分の画像ファイルに変更
+out_image_path = "picture2_detectx.jpg"  # ←ここを自分の画像ファイルに変更
+
+
 # 分類器のパス（OpenCV付属のHaar分類器）
 cascade_path = cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 
@@ -10,11 +15,8 @@ face_cascade = cv2.CascadeClassifier(cascade_path)
 if face_cascade.empty():
     raise IOError("分類器の読み込みに失敗しました。パスを確認してください。")
 
-# 画像ファイルのパスを指定
-image_path = "picture2.jpg"  # ←ここを自分の画像ファイルに変更
-
 # 画像を読み込む
-img = cv2.imread(image_path)
+img = cv2.imread(in_image_path)
 if img is None:
     raise IOError("画像の読み込みに失敗しました。パスを確認してください。")
 
@@ -30,5 +32,8 @@ for (x, y, w, h) in faces:
 
 # 結果を表示
 cv2.imshow('Detected Faces', img)
+# 結果を保存
+cv2.imwrite(out_image_path, img)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
